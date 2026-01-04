9c936e9045a09b17bf3fa132639fc8b74c949d15
Jack Zhuang <9340100@qq.com>
2026-01-04 15:55:22 +0800

    Revert "feat: change to react app"
    This reverts commit 8f08d31ff04103df290e16e587cf858e647fa428.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PlayModule } from './play/play.module';
 import { AuthModule } from './auth/auth.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 
 @Module({
-  imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'client', 'dist'),
-    }),
-    PlayModule, 
-    AuthModule
-  ],
+  imports: [PlayModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
     AppModule,
   );
 
+  app.useStaticAssets(join(__dirname, '..', 'public'));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
