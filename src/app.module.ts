3946ba7aee13639650a4f07f553c4860b71faa70
Jack Zhuang <9340100@qq.com>
2026-01-06 00:00:05 +0800

    rename module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PlayModule } from './play/play.module';
+import { PlayModule } from './interfaces/play.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
